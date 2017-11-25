788964a480f78c2bb50984bf07d462def395408a
주기영 <gyjoo@postech.ac.kr>
2017-11-25 15:53:01 +0900

    Install Truffle and supervisor

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.locals.pretty = true;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.get('/form', function(req,res){
+  res.render('form');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +47,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
